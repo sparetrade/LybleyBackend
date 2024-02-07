25e87080967a00a4f0438d7cededb6fb8b470597
rajeshtiwari2503 <rajeshtiwari2503@gmail.com>
2024-02-07 17:21:35 +0530

    hghfd

--- src/routers/dashboard.js
+++ src/routers/dashboard.js
@@ -5,6 +5,7 @@ const Registration=require("../models/registration");
 const Servicer=require("../models/servicerRegistratin");
 const SubscribedPlan=require("../models/subscribedPlan");
 const Complaint=require("../models/complaint");
+const SubcriptionVerification=require("../models/subscribeVerification");
 
 router.get("/dashboardDetail",async(req,res)=>{
      try{
@@ -13,8 +14,11 @@ router.get("/dashboardDetail",async(req,res)=>{
         let servicer= await Servicer.countDocuments().exec();
         let subscribedPlan= await SubscribedPlan.countDocuments().exec();
         let complaint=await Complaint.countDocuments().exec();
+      //   let verificationData=await SubcriptionVerification.countDocuments().exec();
+        let unVerificationData = await SubcriptionVerification.countDocuments({ status: "ASSIGNED" }).exec();
+        let verificationData = await SubcriptionVerification.countDocuments({ status: "VERIFIED" }).exec();
         let complaintData=await Complaint.find({}); 
-        res.json({plans:plans,users:users,servicer:servicer,subscribedPlan:subscribedPlan,complaint:complaint,complaintData:complaintData});
+        res.json({unVerificationData:unVerificationData,verificationData:verificationData,plans:plans,users:users,servicer:servicer,subscribedPlan:subscribedPlan,complaint:complaint,complaintData:complaintData});
      }catch(err){
         res.status(400).send(err);
      }
